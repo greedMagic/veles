7684fceda84b0636ea5580df1be707e7f4e7c104
Vladimir Ostapov <v.ttbomk@gmail.com>
2019-12-22 17:10:25 +0300

    Завершил верстку. Затрачено 20 часов.

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,5 @@
-import slick from 'slick-carousel';
+import 'slick-carousel';
+import MmenuLight from 'mmenu-light/dist/mmenu-light';
 
 $('.slider__items').slick({
   dots: false,
@@ -7,7 +8,17 @@ $('.slider__items').slick({
   fade: true,
   cssEase: 'linear',
   prevArrow: $('.slider__control.slider__control--prev'),
-  nextArrow: $('.slider__control.slider__control--next')
+  nextArrow: $('.slider__control.slider__control--next'),
+  responsive: [
+    {
+      breakpoint: 575.98,
+      settings: {
+        arrows: false,
+        slidesToShow: 1,
+        dots: true,
+      }
+    },
+  ]
 })
 
 $('.small-slider__items').slick({
@@ -16,5 +27,53 @@ $('.small-slider__items').slick({
   slidesToShow: 4,
   slidesToScroll: 1,
   prevArrow: $('.small-slider__control.small-slider__control--prev'),
-  nextArrow: $('.small-slider__control.small-slider__control--next')
+  nextArrow: $('.small-slider__control.small-slider__control--next'),
+  responsive: [
+    {
+      breakpoint: 575.98,
+      settings: {
+        arrows: false,
+        slidesToShow: 1,
+        dots: true,
+      }
+    },
+    {
+      breakpoint: 767.98,
+      settings: {
+        arrows: false,
+        slidesToShow: 2,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 991.98,
+      settings: {
+        arrows: false,
+        slidesToShow: 2,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 1199.98,
+      settings: {
+        arrows: false,
+        slidesToShow: 3,
+        dots: false
+      }
+    }
+  ]
 })
+
+const $header = $('.header__main-menu');
+let scrollPrev = 0;
+
+$(window).scroll(function () {
+  const scrolled = $(window).scrollTop();
+
+  if (scrolled > 0 && scrolled > scrollPrev) {
+    $header.addClass('header__main-menu--out');
+  } else {
+    $header.removeClass('header__main-menu--out');
+  }
+  scrollPrev = scrolled;
+});
